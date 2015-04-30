00cc5e092540f957991a3e181eaaef4836a2d997
Yaron Naveh <yaronn01@gmail.com>
2015-04-30 22:08:28 +0300

    WIP

--- examples/map.js
+++ examples/map.js
@@ -1,10 +1,29 @@
-var blessed = require('blessed')
-  , contrib = require('../')
-  , screen = blessed.screen()
-  , map = contrib.map({label: 'World Map'})
-    
-screen.append(map)
+var fs = require("fs");
+var path = require("path");
+var watch = require("watch");
+var sys = require('sys');
+var exec = require('child_process').exec;
+var filePath = path.join("../../renewable/panda/weather.json");
+var pandaDir = path.join("../../renewable/panda");
+var recompile_counter = 0;
 
-map.addMarker({"lon" : "-79.0000", "lat" : "37.5000", color: "red", char: "X" })
+var oohDatMap = function() {
+  fs.readFile(filePath, { encoding: 'utf-8'}, function(err, data) {
+    var weather_data = JSON.parse(data);
+    var coords = weather_data["coord"];
+    var lon = coords["lon"];
+    var lat = coords["lat"];
 
-screen.render()
+    var blessed = require('blessed')
+      , contrib = require('../')
+      , screen = blessed.screen()
+      , map = contrib.map({label: 'World Map'});
+
+    screen.append(map);
+    map.addMarker({"lon" : lon, "lat" : lat, color: "red", char: "X" });
+
+    screen.render();
+  });
+}
+
+oohDatMap();
